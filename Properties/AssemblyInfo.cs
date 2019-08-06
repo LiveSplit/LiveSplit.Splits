dfb3e201572f2053967e8fbf14ad70ff78be2be8
Peter Halasz <peter@becauseofgames.com>
2019-08-06 11:54:16 +1000

    Make internals visible for unit tests

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -38,3 +38,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: ComponentFactory(typeof(SplitsComponentFactory))]
+[assembly: InternalsVisibleTo("LiveSplit.UnitTests")]
