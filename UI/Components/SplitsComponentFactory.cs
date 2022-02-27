3272c3e3e3ae992988369f0ad112562c6f76201f
wooferzfg <spapushin@gmail.com>
2022-02-27 17:52:55 -0500

    Update version to 1.8.19

--- UI/Components/SplitsComponentFactory.cs
+++ UI/Components/SplitsComponentFactory.cs
@@ -19,6 +19,6 @@ namespace LiveSplit.UI.Components
 
         public string UpdateURL => "http://livesplit.org/update/";
 
-        public Version Version => Version.Parse("1.8.10");
+        public Version Version => Version.Parse("1.8.19");
     }
 }
