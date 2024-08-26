0c9db0e21852dd8eb956d5081fb79e861dbf5521
10_days_till_xmas <159474213+10-days-till-xmas@users.noreply.github.com>
2024-08-26 01:04:48 +0100

    Clean up DeltaSplitTimeFormatter

--- src/LiveSplit.Splits/TimeFormatters/DeltaSplitTimeFormatter.cs
+++ src/LiveSplit.Splits/TimeFormatters/DeltaSplitTimeFormatter.cs
@@ -2,26 +2,14 @@
 
 namespace LiveSplit.TimeFormatters
 {
-    public class DeltaSplitTimeFormatter : ITimeFormatter
+    public class DeltaSplitTimeFormatter : GeneralTimeFormatter
     {
-        public TimeAccuracy Accuracy { get; set; }
-        public bool DropDecimals { get; set; }
-
         public DeltaSplitTimeFormatter(TimeAccuracy accuracy, bool dropDecimals)
         {
             Accuracy = accuracy;
             DropDecimals = dropDecimals;
-        }
-        public string Format(TimeSpan? time)
-        {
-            var deltaTime = new DeltaTimeFormatter();
-            deltaTime.Accuracy = Accuracy;
-            deltaTime.DropDecimals = DropDecimals;
-            var formattedTime = deltaTime.Format(time);
-            if (time == null)
-                return TimeFormatConstants.DASH;
-            else
-                return formattedTime;
+            NullFormat = NullFormat.Dash;
+            ShowPlus = true;
         }
     }
 }
